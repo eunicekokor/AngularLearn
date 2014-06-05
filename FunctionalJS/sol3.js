de176b72caa1d8f3ebc329da16680af6d9623739
eunicekokor <eunicekokor@gmail.com>
2014-06-04 23:44:22 -0700

    two max

--- FunctionalJS/sol3.js
+++ FunctionalJS/sol3.js
@@ -1,6 +1,13 @@
 function doubleAll(numbers) {
-	numbers.prototype.map = function(){
-		return 2 * numbers[0]})
-    }
+      var map = Array.prototype.map;
+      var result = map.call(numbers, function (x) {return 2*x; })
+      return result;
+}
     
     module.exports = doubleAll
+
+/**********
+Lessons Learned:
+1) map = Array.prototype.map is just a formality. you just need
+array.map(function) to utilize the map() method. DUHHHHHHHHH.
+***********/
