7a9f75e738e51288cd075acc3a9d58774ae79641
eunicekokor <eunicekokor@gmail.com>
2014-08-04 13:38:22 -0700

    finished the fifth node challenge

--- FunctionalJS/sol5.js
+++ FunctionalJS/sol5.js
@@ -1,7 +1,11 @@
 function checkUsersValid(goodUsers) {
-      return function(submittedUsers) {
-        // SOLUTION GOES HERE
-      };
-    }
-    
-    module.exports = checkUsersValid
+    return function(submittedUsers) {
+    	return submittedUsers.every(function(submittedUser) {
+    		return goodUsers.some(function(goodUser) {
+    			return goodUser.id === submittedUser.id
+    		})
+    	})
+    };
+}
+
+module.exports = checkUsersValid
